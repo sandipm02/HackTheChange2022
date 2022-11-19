2617a50e0e3b088e54135ab09d50ef076e903b06
Sandip Mishra <sandip@Sandips-MacBook-Pro.local>
2022-11-19 15:31:20 -0700

    added section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LandingPage from "./components/landingPage";
 import Socials from "./components/Socials";
 import Content from "./components/Content";
 import Features from "./components/Features";
+import Blog from "./components/Blog";
 
 function App() {
   return (
