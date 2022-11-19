bd5ada6b676d0e77b2c1fd42058ec6f20d1d2472
luqmanbukhari <luqmanbukhari@gmail.com>
2022-11-19 12:45:55 -0700

    Testing commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import LoginPage from "./components/LoginPage";
-
+//TEST COMMIT
 function App() {
   return (
     <div className="App">
-
       <h1 className="text-3xl font-bold underline">
-        <LoginPage/>
+        <LoginPage />
       </h1>
     </div>
   );
