c7226ad4efb8513826232dd35f03cab017460f33
Sandip Mishra <sandip@Sandips-MacBook-Pro.local>
2022-11-19 14:06:44 -0700

    added content section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import LoginPage from "./components/LoginPage";
 import LandingPage from "./components/landingPage";
 import Socials from "./components/Socials";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
         <LandingPage/>
         <Socials/>
+        <Content/>
     </div>
   );
 }
