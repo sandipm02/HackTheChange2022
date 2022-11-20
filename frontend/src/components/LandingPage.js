f2b33c19ea645f1b626e3855bb4b9d418e960601
zhifanl <zhifanli2000@gmail.com>
2022-11-20 07:40:19 -0700

    .,

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -11,14 +11,15 @@ function LandingPage() {
                         <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">By removing the chances for <strong>hidden biases</strong> and allowing companies to approach quality talent, we can contribute toward building an <strong>equal and efficient</strong> hiring landscape.</p>
                         <a href="#" class="learn-more-hero inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                             Learn More
-                        </a> 
-                        <a href="#" class="login-button-hero inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                            Login
-                        </a> 
+                        </a>
+                        <a href="/main" class="login-button-hero inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                                Log In
+                        </a>
+
                     </div>
                     <div class="hero-image lg:mt-0 lg:col-span-5 lg:flex">
                         <img src={require('../assets/phone-mockup.png')} />
-                    </div>                
+                    </div>
                 </div>
             </section>
         </div>
